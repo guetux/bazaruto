2faea3f23269cd308cec9ee3434d4183b07ee88e
Stefan Reinhard <sr@feinheit.ch>
2012-04-11 11:17:41 +0200

    everywhere, damit

--- src/ch/bazaruto/tests/BazarutoTests.java
+++ src/ch/bazaruto/tests/BazarutoTests.java
@@ -13,36 +13,36 @@ import ch.bazaruto.Response;
 import ch.bazaruto.tests.controllers.SimpleController1;
 
 public class BazarutoTests {
-	
-	public Bazaruto bazaruto = new Bazaruto();
+    
+    public Bazaruto bazaruto = new Bazaruto();
 
-	@Before
-	public void setUp() throws Exception {		
-		bazaruto.addController(SimpleController1.class);
-	}
+    @Before
+    public void setUp() throws Exception {        
+        bazaruto.addController(SimpleController1.class);
+    }
 
-	@Test
-	public void testIndex() {
-		Request req = new Request("/sc1/", "get", null, null, null);
-		Response res = bazaruto.dispatch(req);
-		assertEquals("SC1:index", res.getData());
-	}
+    @Test
+    public void testIndex() {
+        Request req = new Request("/sc1/", "get", null, null, null);
+        Response res = bazaruto.dispatch(req);
+        assertEquals("SC1:index", res.getData());
+    }
 
-	@Test
-	public void testDetail() {
-		Properties parms = new Properties();
-		parms.put("id", 1);
-		Request req = new Request("/sc1/details", "get", null, parms, null);
-		Response res = bazaruto.dispatch(req);
-		assertEquals("SC1:detail:1", res.getData());
-	}
-	
-	@Test
-	public void testTimesTwo() {
-		Properties parms = new Properties();
-		parms.put("number", 21);
-		Request req = new Request("/sc1/timestwo", "post", null, parms, null);
-		Response res = bazaruto.dispatch(req);
-		assertEquals("SC1:timestwo:42", res.getData());
-	}
+    @Test
+    public void testDetail() {
+        Properties parms = new Properties();
+        parms.put("id", 1);
+        Request req = new Request("/sc1/details", "get", null, parms, null);
+        Response res = bazaruto.dispatch(req);
+        assertEquals("SC1:detail:1", res.getData());
+    }
+    
+    @Test
+    public void testTimesTwo() {
+        Properties parms = new Properties();
+        parms.put("number", 21);
+        Request req = new Request("/sc1/timestwo", "post", null, parms, null);
+        Response res = bazaruto.dispatch(req);
+        assertEquals("SC1:timestwo:42", res.getData());
+    }
 }
